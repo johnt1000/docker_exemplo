386d365a4bd0bdffa1111d5e33f691fb53034b46
johnt1000 <jonatas.rodriguess@gmail.com>
2018-11-09 11:15:46 -0300

    Ajuster para testes de comunicação.

--- spa/src/main.js
+++ spa/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
 
 new Vue({
   render: h => h(App),
